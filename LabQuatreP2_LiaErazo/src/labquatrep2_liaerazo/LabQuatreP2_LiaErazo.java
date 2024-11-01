d9257ea34fa633fb7d645071e092f937617e15c7
liaerazo16 <dariellemilla@gmail.com>
2024-11-01 14:58:10 -0600

    Create

---
+++ LabQuatreP2_LiaErazo/src/labquatrep2_liaerazo/LabQuatreP2_LiaErazo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labquatrep2_liaerazo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paola
+ */
+public class LabQuatreP2_LiaErazo {
+    static Scanner leer = new Scanner(System.in);
+    static ArrayList<String> usuarios = new ArrayList<>();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
